44451ed830d8967d8cc1664ecd770490f871deb0
lewis123cater <124276991+lewis123cater@users.noreply.github.com>
2025-05-09 09:04:05 +0100

    redirect images to s3

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,12 +4,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "pm-s3-images.s3.us-east-2.amazonaws.com",
+        hostname: "pm-s3-images-lc.s3.us-east-1.amazonaws.com",
         port: "",
         pathname: "/**",
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
 
 export default nextConfig;
